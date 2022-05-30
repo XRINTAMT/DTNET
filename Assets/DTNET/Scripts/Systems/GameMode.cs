2d6d0ff21e9453a7bf34199831c129ead0b1ede7
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2022-05-30 18:03:46 +0200

    Script for handling selected game mode and Load different scenes

---
+++ Assets/DTNET/Scripts/Systems/GameMode.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DTNET.CSystem 
+{
+    public static class GameMode
+    {
+        public static string SelectedMode { get; set; }
+    }
+}
+    
