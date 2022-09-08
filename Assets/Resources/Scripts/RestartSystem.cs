766a4f0e399640a182cc1658751c5fe61ca1c647
Oleg-A-LLIto <Fedol1408@gmail.com>
2022-09-08 19:01:50 +0300

    First version of restart system (remote is there for debugging))

--- Assets/Resources/Scripts/RestartSystem.cs
+++ Assets/Resources/Scripts/RestartSystem.cs
@@ -5,11 +5,31 @@ using UnityEngine;
 public class RestartSystem : MonoBehaviour
 {
     [SerializeField] GameObject Changables;
-    GameObject Save;
+    [SerializeField] FadeMessageManager Fade;
+    GameObject SavedState;
+
 
     void Start()
     {
-        
+        Save();
+    }
+
+    public void Save(){
+        if(SavedState != null){
+            Object.Destroy(SavedState);
+        }
+        SavedState = Object.Instantiate(Changables);
+        SavedState.SetActive(false);
+    }
+
+    public void Load(string text = ""){
+        if(SavedState == null){
+            return;
+        }
+        Fade.FadeWithText(text);
+        Object.Destroy(Changables);
+        Changables = Object.Instantiate(SavedState);
+        Changables.SetActive(true);
     }
 
     void Update()
