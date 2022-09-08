b3610495eead314bf29b755119ffab2dacdc62db
Oleg-A-LLIto <Fedol1408@gmail.com>
2022-09-08 14:30:29 +0300

    Added save system

---
+++ Assets/Resources/Scripts/RestartSystem.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartSystem : MonoBehaviour
+{
+    [SerializeField] GameObject Changables;
+    GameObject Save;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
