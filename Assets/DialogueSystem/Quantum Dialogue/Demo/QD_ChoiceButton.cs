f1dc22b789c871f7b7eb00904cef827e3854f1a7
Roman <murman89@yandex.ru>
2022-09-15 02:15:06 +0300

    dialogue Choose System

--- Assets/DialogueSystem/Quantum Dialogue/Demo/QD_ChoiceButton.cs
+++ Assets/DialogueSystem/Quantum Dialogue/Demo/QD_ChoiceButton.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace QuantumTek.QuantumDialogue.Demo
 {
@@ -6,7 +7,26 @@ namespace QuantumTek.QuantumDialogue.Demo
     {
         public int number;
         public QD_DialogueDemo demo;
- 
+        public string text;
+        public GameObject dialogue;
+        private void Start()
+        {
+            text = GetComponent<Text>().text;
+        }
+
+        public void SelectButton() 
+        {
+            if (text!= "Close")
+            {
+                demo.Choose(number);
+            }
+            if (text == "Close")
+            {
+                Debug.Log("Close");
+                dialogue.SetActive(false);
+            }
+     
+        }
         public void Select() => demo.Choose(number);
     }
 }
