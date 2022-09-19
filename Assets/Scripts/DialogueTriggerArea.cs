b135b1839d72218e2ccdcf4aa7492c6936a10c83
Roman <murman89@yandex.ru>
2022-09-19 16:20:26 +0300

    update

---
+++ Assets/Scripts/DialogueTriggerArea.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTriggerArea : MonoBehaviour
+{
+    public bool inArea;
+    [SerializeField] private GameObject dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void InArea()
+    {
+        inArea = true;
+    }
+    public void OutArea()
+    {
+        inArea = false;
+    }
+    public void activateDialogue() 
+    {
+        if (inArea)
+        {
+            dialogue.SetActive(true);
+        }
+    }
+   
+}
