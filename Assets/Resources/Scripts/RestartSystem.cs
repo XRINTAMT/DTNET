5f7d68afa897d24676af89f7f30c85a78368989f
Oleg-A-LLIto <Fedol1408@gmail.com>
2022-09-28 12:53:57 +0300

    Added a delay so that loading moment would match the 'blackout" state

--- Assets/Resources/Scripts/RestartSystem.cs
+++ Assets/Resources/Scripts/RestartSystem.cs
@@ -21,14 +21,26 @@ public class RestartSystem : MonoBehaviour
         SavedState.SetActive(false);
     }
 
-    public void Load(string text = ""){
-        if(SavedState == null){
+    public void Load(string text = "") {
+        if (SavedState == null) {
             return;
+        }
+        StartCoroutine(LoadDelay());
+        foreach(FadeMessageManager Fade in FindObjectsOfType<FadeMessageManager>())
+        {
+            Fade.FadeWithText(text);
+        }
+    }
+
+    IEnumerator LoadDelay()
+    {
+        for(float i = 0; i < 2; i += Time.deltaTime)
+        {
+            yield return 0;
         }
         Object.Destroy(Changables);
         Changables = Object.Instantiate(SavedState);
         Changables.SetActive(true);
-        FindObjectOfType<FadeMessageManager>().FadeWithText(text);
     }
 
     void Update()
