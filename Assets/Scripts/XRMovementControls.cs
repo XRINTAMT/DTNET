5d61ab42d2d126c8710321bb04715423f6f28512
Roman <murman89@yandex.ru>
2022-10-07 16:06:30 +0300

    fixed locomation

--- Assets/Scripts/XRMovementControls.cs
+++ Assets/Scripts/XRMovementControls.cs
@@ -2,35 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Autohand.Demo;
+using Autohand;
 
 public class XRMovementControls : MonoBehaviour
 {
     [SerializeField] XRHandPlayerControllerLink MovementControls;
     [SerializeField] GameObject Teleport;
-    [SerializeField] XRHandControllerLink RealHand;
-    [SerializeField] XRHandControllerLink DummyHand;
+
     void Awake()
     {
-        int MovementType = PlayerPrefs.GetInt("MovementType", 0);
-        Debug.Log("Loaded movement type as " + MovementType);
-        SwitchLocomotion(MovementType);
+        //int MovementType = PlayerPrefs.GetInt("MovementType", 0);
+        //Debug.Log("Loaded movement type as " + MovementType);
+        //SwitchLocomotion(MovementType);
     }
 
     public void SwitchLocomotion(int type)
     {
         switch (type)
         {
-            case (-1):
-                Teleport.SetActive(false);
-                MovementControls.moveController = DummyHand;
-                break;
+            //case (-1):
+            //    Teleport.SetActive(false);
+            //    MovementControls.moveController = DummyHand;
+            //    break;
             case (0):
+                AutoHandPlayer.teleportMove = true;
                 Teleport.SetActive(true);
-                MovementControls.moveController = DummyHand;
+                //MovementControls.moveController = DummyHand;
                 break;
             case (1):
+                AutoHandPlayer.teleportMove = false;
                 Teleport.SetActive(false);
-                MovementControls.moveController = RealHand;
+                //MovementControls.moveController = RealHand;
                 break;
         }
     }
