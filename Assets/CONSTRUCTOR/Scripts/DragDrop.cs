c23c9ffcc96982b2d0c90dcee2111fada950191d
Roman <murman89@yandex.ru>
2022-10-24 16:12:45 +0300

    Nodes lines

--- Assets/CONSTRUCTOR/Scripts/DragDrop.cs
+++ Assets/CONSTRUCTOR/Scripts/DragDrop.cs
@@ -8,21 +8,37 @@ public class DragDrop : MonoBehaviour
     private Vector3 offset;
 
 
-    public float verticalOffset;
-    public float horizontalOffset;
+    public float OffsetX;
+    public float OffsetY;
+    public float OffsetZ;
 
-    public Transform panel;
-
+    Vector3 pos;
+    float zCameraDistance;
+    private void Start()
+    {
+        zCameraDistance=Camera.main.WorldToScreenPoint(transform.position).z;
+    }
     void Update()
     {
-        var position = (Vector2)Input.mousePosition + (Vector2.up * verticalOffset) + (Vector2.left * horizontalOffset);
-        panel.position = position;
+        //var position = (Vector2)Input.mousePosition + (Vector2.up * verticalOffset) + (Vector2.left * horizontalOffset);
+        //transform.position = position;
+
+        //pos = new Vector3(Input.mousePosition.x + verticalOffset, Input.mousePosition.y + horizontalOffset, zCameraDistance);
+        //Vector3 newWorldPos = Camera.main.ScreenToWorldPoint(pos);
+        //transform.position = pos;
+
+
+        Vector3 mousePosition =Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.y + OffsetZ));
+        transform.position = mousePosition;
+
+        //pos = new Vector3(Input.mousePosition.x + verticalOffset, Input.mousePosition.y+horizontalOffset, 1) ;
+        //transform.position = pos;
     }
     void OnMouseDown()
     {
         Debug.Log(6);
-        var position = (Vector2)Input.mousePosition + (Vector2.up * verticalOffset) + (Vector2.left * horizontalOffset);
-        panel.position = position;
+        var position = (Vector2)Input.mousePosition + (Vector2.up * OffsetX) + (Vector2.left * OffsetY);
+        transform.position = position;
 
         //offset = gameObject.transform.position -
         //    Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f));
