9029bb0cb395b000835117ebe38f0978147c4f6c
Nicklas Jonsson <jonsson.nicklas1990@gmail.com>
2022-10-25 14:34:46 +0200

    limit to MAX 5 tubes

--- Assets/DTNET/Scripts/Actions/TubeSpawner.cs
+++ Assets/DTNET/Scripts/Actions/TubeSpawner.cs
@@ -7,11 +7,17 @@ namespace DTNET.Actions {
     {
         [SerializeField] private GameObject tubePrefab;
 
+        private const int MAX_NUM_OF_TUBES = 5;
+        private int currentNumberOfTubes = 0;
+
         public void spawnTubeAtPosition() 
         {
-            float y_offset = 0.45f;
-            Vector3 spawnPosition = transform.position + new Vector3(0, y_offset, 0);
-            GameObject tube = Instantiate(tubePrefab, spawnPosition, Quaternion.identity);
+            if(currentNumberOfTubes < MAX_NUM_OF_TUBES) {
+                float y_offset = 0.45f;
+                Vector3 spawnPosition = transform.position + new Vector3(0, y_offset, 0);
+                GameObject tube = Instantiate(tubePrefab, spawnPosition, Quaternion.identity);
+                currentNumberOfTubes++;
+            }
         }
     }
 }
