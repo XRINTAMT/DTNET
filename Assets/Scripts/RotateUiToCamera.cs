2c343931ae5290b296d014b91f9221274cff4d50
Roman <murman89@yandex.ru>
2022-11-02 19:48:41 +0300

    Made outline work/rotatin giude panels/block teleportation on bed

--- Assets/Scripts/RotateUiToCamera.cs
+++ Assets/Scripts/RotateUiToCamera.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotateDialogue : MonoBehaviour
+public class RotateUiToCamera : MonoBehaviour
 {
 
     [SerializeField] private Camera cam;
