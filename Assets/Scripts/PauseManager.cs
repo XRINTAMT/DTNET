4bf468d4a33a9d9341ed6cc6b1856e480c8dbf75
Roman <murman89@yandex.ru>
2022-12-07 18:54:29 +0300

    Fixed movement in tutorial/add point in menu

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -66,7 +66,7 @@ public class PauseManager : MonoBehaviour
         PauseMenu.transform.position = MenuOffset.transform.position;
         PauseMenu.transform.rotation = MenuOffset.transform.rotation;
         PauseMenu.transform.rotation = Quaternion.Euler(new Vector3(MenuOffset.transform.rotation.eulerAngles.x, MenuOffset.transform.rotation.eulerAngles.y, 0));
-        Controls.SwitchLocomotion(-1);
+
         if (Controls!=null) Controls.SwitchLocomotion(-1);
       
         GetComponent<UnscaleMove>().Pause();
