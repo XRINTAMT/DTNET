d2e2546012958955a95c0af53cef05017db03d79
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-01-02 15:19:03 +0300

    Reordered the columns in the spreadsheet

--- Assets/QuestionSystem/Question.cs
+++ Assets/QuestionSystem/Question.cs
@@ -12,20 +12,24 @@ namespace QuestionSystem
             Tag = values[0];
             valuesText[0] = values[1];
             valuesText[1] = values[2];
-            valuesText[2] = values[3];
-            valuesText[3] = values[4];
-            valuesText[4] = values[5];
-            MoodChanges = int.Parse(values[6]);
-            AnimationType = values[7];
+            
+            MoodChanges = int.Parse(values[3]);
+            AnimationType = values[4];
             Prerequisite = null;
             IsAsked = 0;
-            valuesText[5] = values[8];
-            valuesText[6] = values[9];
-            valuesText[7] = values[10];
-            valuesText[8] = values[11];
-            valuesText[9] = values[12];
-            PrerequisiteTag = values[13];
-            Topic = values[14];
+            PrerequisiteTag = values[5];
+            Topic = values[6];
+            InformationTag = values[7];
+
+            valuesText[2] = values[8];
+            valuesText[3] = values[9];
+            valuesText[4] = values[10];
+            valuesText[5] = values[11];
+            valuesText[6] = values[12];
+            valuesText[7] = values[13];
+            valuesText[8] = values[14];
+            valuesText[9] = values[15];
+            
         }
         [field: SerializeField] public string Tag { get; set; }
         [field: SerializeField] public string Topic { get; set; }
@@ -36,6 +40,7 @@ namespace QuestionSystem
         [field: SerializeField] public int IsAsked { get; set; }
         [field: SerializeField] public Dictionary<string, string> Answer { get; set; }
         [field: SerializeField] public string PrerequisiteTag { get; set; }
+        [field: SerializeField] public string InformationTag { get; set; }
         public bool PrerequisiteMet()
         {
             return Prerequisite == null || (Prerequisite.IsAsked > 0);
@@ -49,14 +54,14 @@ namespace QuestionSystem
             Answer = new Dictionary<string, string>();
             //populate dictionaries from arrays because Unity can't serialize dictionaries
             Text["English"] = valuesText[0];
-            Text["German"] = valuesText[1];
-            Text["Swedish"] = valuesText[2];
-            Text["Lithuanian"] = valuesText[3];
-            Text["Latvian"] = valuesText[4];
-            Answer["English"] = valuesText[5];
-            Answer["German"] = valuesText[6];
-            Answer["Swedish"] = valuesText[7];
-            Answer["Lithuanian"] = valuesText[8];
+            Answer["English"] = valuesText[1];
+            Text["German"] = valuesText[2];
+            Answer["German"] = valuesText[3];
+            Text["Swedish"] = valuesText[4];
+            Answer["Swedish"] = valuesText[5];
+            Text["Lithuanian"] = valuesText[6];
+            Answer["Lithuanian"] = valuesText[7];
+            Text["Latvian"] = valuesText[8];
             Answer["Latvian"] = valuesText[9];
         }
     }
