1755451cc3126f6c40b3b31d0891e67e4028b4a8
Roman <murman89@yandex.ru>
2023-01-03 17:56:28 +0300

    End of scenario sum up screen/Edit rooms

---
+++ Assets/Scripts/Scenario2/CompletedScenario.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CompletedScenario : MonoBehaviour
+{
+    [SerializeField] GameObject panelCompletedScenario;
+    [SerializeField] Image moodStatus;
+    [SerializeField] List<Sprite> moodList;
+    [SerializeField] Text answersProgress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void SetData(int indexMoodList, Text textProgress) 
+    {
+        panelCompletedScenario.SetActive(true);
+        moodStatus.sprite = moodList[indexMoodList];
+        answersProgress.text = textProgress + "%";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
