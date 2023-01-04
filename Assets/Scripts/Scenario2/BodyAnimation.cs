4b05ee02c92fcd0db90b8ad142412dde2cd61426
Roman <murman89@yandex.ru>
2023-01-04 17:49:13 +0300

    add body animation controller

--- Assets/Scripts/Scenario2/BodyAnimation.cs
+++ Assets/Scripts/Scenario2/BodyAnimation.cs
@@ -10,43 +10,74 @@ public class BodyAnimation : MonoBehaviour
     {
         
     }
-    public void SetBadMoodAnimation() 
+    public void SetBadMoodAnimation(string triggerName) 
     {
-        int rand = Random.Range(1, 2);
-        switch (rand)
-        {
-            case 1:
-                animator.SetTrigger("BadMood1");
-                break;
-            case 2:
-                animator.SetTrigger("BadMood2");
-                break;
-            case 3:
-                animator.SetTrigger("BadMood3");
-                break;
-            default:
-                break;
-        }
+        animator.SetTrigger(triggerName);
 
+        if (triggerName == null)
+        {
+            int rand = Random.Range(1, 4);
+            switch (rand)
+            {
+                case 1:
+                    animator.SetTrigger("BadMood1");
+                    break;
+                case 2:
+                    animator.SetTrigger("BadMood2");
+                    break;
+                case 3:
+                    animator.SetTrigger("BadMood3");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
-    public void SetBadGoodAnimation()
+    public void SetBadGoodAnimation(string triggerName)
     {
-        int rand = Random.Range(1, 2);
-        switch (rand)
-        {
-            case 1:
-                animator.SetTrigger("GoodMood1");
-                break;
-            case 2:
-                animator.SetTrigger("GoodMood2");
-                break;
-            case 3:
-                animator.SetTrigger("GoodMood3");
-                break;
-            default:
-                break;
-        }
+        animator.SetTrigger(triggerName);
 
+        if (triggerName == null)
+        {
+            int rand = Random.Range(1, 4);
+            switch (rand)
+            {
+                case 1:
+                    animator.SetTrigger("GoodMood1");
+                    break;
+                case 2:
+                    animator.SetTrigger("GoodMood2");
+                    break;
+                case 3:
+                    animator.SetTrigger("GoodMood3");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public void SetNeutrallAnimation(string triggerName)
+    {
+        animator.SetTrigger(triggerName);
+
+        if (triggerName==null)
+        {
+            int rand = Random.Range(1, 4);
+            switch (rand)
+            {
+                case 1:
+                    animator.SetTrigger("NeutralMood1");
+                    break;
+                case 2:
+                    animator.SetTrigger("NeutralMood2");
+                    break;
+                case 3:
+                    animator.SetTrigger("NeutralMood3");
+                    break;
+                default:
+                    break;
+            }
+        }
     }
     // Update is called once per frame
     void Update()
