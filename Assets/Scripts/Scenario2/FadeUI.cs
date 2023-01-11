1b236143d33dad55881c17cd2ee2fc7194718c78
Roman <murman89@yandex.ru>
2023-01-12 01:09:07 +0300

    Fade UI in scenario2

---
+++ Assets/Scripts/Scenario2/FadeUI.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] List<CanvasGroup> canvasGroups;
+    public bool fadeIn,fadeOut;
+
+    void Start()
+    {
+        
+    }
+
+    public void FadeIn() 
+    {
+        fadeIn = true;
+    }
+    public void FadeOut()
+    {
+        fadeOut = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (fadeIn)
+        {
+            for (int i = 0; i < canvasGroups.Count; i++)
+            {
+                if (canvasGroups[i].alpha > 0)
+                {
+                    canvasGroups[i].alpha -= 1f * Time.deltaTime;
+                }
+            }
+            if (canvasGroups[canvasGroups.Count-1].alpha<=0)
+            {
+                fadeIn = false;
+            }
+        }
+
+        if (fadeOut)
+        {
+            for (int i = 0; i < canvasGroups.Count; i++)
+            {
+                if (canvasGroups[i].alpha < 1)
+                {
+                    canvasGroups[i].alpha += 1f *Time.deltaTime;
+                }
+            }
+            if (canvasGroups[canvasGroups.Count - 1].alpha >= 1)
+            {
+                fadeOut = false;
+            }
+        }
+    }
+}
