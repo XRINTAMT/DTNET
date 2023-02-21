5e0e43842115add8db7d4732e1bf59b88a79c1c8
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-02-21 19:13:20 +0300

    A fully functional observation sheet

--- Assets/Resources/Prefabs/ObservationSheet/MultipleChoiceBehaviour.cs
+++ Assets/Resources/Prefabs/ObservationSheet/MultipleChoiceBehaviour.cs
@@ -1,18 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using ScenarioTaskSystem;
 
 public class MultipleChoiceBehaviour : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int[] correctValues;
+    [SerializeField] MultipleChoiceRow[] options;
+    [SerializeField] MultipleChoiceRow renderResult;
+    [SerializeField] Date Datestamp;
+    [SerializeField] Date Timestamp;
+    [SerializeField] GameObject MultupleChoiceUI;
+    [SerializeField] GameObject Next;
+
+    void Awake()
+    {
+        GameObject ChangableItemsContainer = findParentWithTag("ChangableItems");
+        if (!ChangableItemsContainer.name.Contains("(Clone)"))
+        {
+            int correctRow = (int)Mathf.Floor(Random.Range(0, options.Length));
+            for (int i = 0; i < options.Length; i++)
+            {
+                options[i].RenderObservation(new Observation(correctValues, (i != correctRow)));
+            }
+        }
+    }
+
+    public void ChooseAnswer(Observation _o)
+    {
+        renderResult.gameObject.SetActive(true);
+        renderResult.RenderObservation(_o);
+        Datestamp.GenerateDatestamp();
+        Timestamp.GenerateTimeStamp();
+        MultupleChoiceUI.SetActive(false);
+        if(Next != null)
+            Next.SetActive(true);
+        GetComponent<Task>().Complete((_o.wrong) ? 0 : 1);
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private GameObject findParentWithTag(string _tag)
     {
-        
+        var parent = gameObject.transform.parent;
+        while(parent != null) {
+            if (parent.tag == _tag)
+            {
+                return parent.gameObject as GameObject;
+            }
+            parent = parent.transform.parent;
+        }
+        return null;
+    }
+}
+
+[System.Serializable]
+public class Observation
+{
+    int Respitations;
+    int Scale1;
+    int Scale2;
+    int Oxygen;
+    int BloodPressure;
+    int Pulse;
+    int Consciousness;
+    int Temperature;
+    public bool wrong;
+    public int[] values;
+    static int[] valueLimits = { 6, 3, 7, 3, 13, 12, 4, 5, 2};
+
+    /*
+    public Observation(int _r, int _s1, int _s2, int _o, int _bp, int _p, int _c, int _t, bool _isWrong)
+    {
+
+    }
+    */
+
+    public Observation(int[] _values, bool _isWrong)
+    {
+        values = (int[])_values.Clone();
+        wrong = false;
+        //randomize the values for the wrong one
+        if (_isWrong)
+        {
+            wrong = true;
+            int r1, r2;
+            r1 = (int)Mathf.Floor(Random.Range(0, _values.Length));
+            do
+            {
+                r2 = (int)Mathf.Floor(Random.Range(0, _values.Length));
+            }
+            while (r1 == r2);
+            do
+            {
+                values[r1] = (int)Mathf.Round(Random.Range(0, valueLimits[r1]));
+            }
+            while (values[r1] == _values[r1]);
+            do
+            {
+                values[r2] = (int)Mathf.Round(Random.Range(0, valueLimits[r2]));
+            }
+            while (values[r2] == _values[r2]);
+        }
     }
 }
---
+++ Assets/Resources/Prefabs/ObservationSheet/MultipleChoiceRow.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MultipleChoiceRow : MonoBehaviour
+{
+    [SerializeField] MultipleChoiceBehaviour choice;
+    [SerializeField] GameObject[] Ticks;
+    [SerializeField] Text OxygenPerMinute;
+    [SerializeField] Text EscalationOfCare;
+    [SerializeField] Observation obs;
+
+    public void RenderObservation(Observation _o)
+    {
+        obs = new Observation(_o.values, _o.wrong);
+        for (int i = 0; i <= 2; i++)
+        {
+            PlaceTick(i, _o.values[i]);
+        }
+        if (_o.values[3] == 0)
+            OxygenPerMinute.gameObject.SetActive(false);
+        else
+        {
+            Ticks[3].SetActive(false);
+            OxygenPerMinute.text = _o.values[3].ToString();
+        }
+        if (_o.values[4] <= 5)
+        {
+            Ticks[5].SetActive(false);
+            PlaceTick(4, _o.values[4]);
+        }
+        else
+        {
+            Ticks[4].SetActive(false);
+            PlaceTick(5, _o.values[4]-6);
+        }
+        for (int i = 5; i < _o.values.Length-1; i++)
+        {
+            PlaceTick(i+1, _o.values[i]);
+        }
+        EscalationOfCare.text = (_o.values[_o.values.Length - 1] == 0) ? "N" : "Y";
+    }
+
+    private void PlaceTick(int _tickId, int _position)
+    {
+        Ticks[_tickId].GetComponent<RectTransform>().localPosition = new Vector3(Ticks[_tickId].GetComponent<RectTransform>().localPosition.x, Ticks[_tickId].GetComponent<RectTransform>().localPosition.y - (_position * 27.25f), Ticks[_tickId].GetComponent<RectTransform>().localPosition.z);
+    }
+
+    public void Submit()
+    {
+        choice.ChooseAnswer(obs);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
