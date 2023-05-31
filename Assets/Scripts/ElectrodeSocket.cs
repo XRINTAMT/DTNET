aa76c8ccf1ee1c1926a0f729295febda107fb1e0
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-06-01 00:06:36 +0300

    Fake attachments and new (correct) electrode mechanics

---
+++ Assets/Scripts/ElectrodeSocket.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Autohand;
+
+public class ElectrodeSocket : MonoBehaviour
+{
+    [SerializeField] int RequiredPadID;
+    Electrode ConnectedElectrode;
+
+    void Start()
+    {
+        
+    }
+
+    public bool IsConnectedCorrectly()
+    {
+        if(ConnectedElectrode == null)
+        {
+            if (RequiredPadID == -1)
+                return true;
+            else
+                return false;
+        }
+        return (ConnectedElectrode.ID == RequiredPadID);
+    }
+
+    public void Connect()
+    {
+        Invoke("ProcessConnection", Time.fixedDeltaTime);
+    }
+
+    private void ProcessConnection()
+    {
+        Electrode _e = GetComponentInChildren<Electrode>();
+        if (_e != null)
+        {
+            ConnectedElectrode = _e;
+            _e.TryGetComponent<PlacePoint>(out PlacePoint _pp);
+            if (_pp != null)
+            {
+                _pp.enabled = true;
+            }
+        }
+        else
+        {
+            Debug.Log("Somehow there's no Electrode here");
+        }
+    }
+
+    public void Disconnect()
+    {
+        ConnectedElectrode = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
