9168b1d296014e9649e7dc2d437667f5569e4106
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-06-02 12:25:10 +0300

    Fix: packaging could sometimes just fall apart

--- Assets/Scripts/Packaging.cs
+++ Assets/Scripts/Packaging.cs
@@ -13,33 +13,49 @@ public class Packaging : MonoBehaviour
 
     public void Start()
     {
-        
+        Rigidbody _rb = Content.GetComponent<Rigidbody>();
+        _rb.detectCollisions = false;
+        _rb.angularDrag = 0;
+        _rb.mass = 0;
     }
 
     public void OnUnpacked()
     {
+        Content.gameObject.AddComponent<Rigidbody>();
         Content.enabled = true;
         isUnpacked = true;
         ConfigurableJoint CJ = Content.GetComponent<ConfigurableJoint>();
         if(CJ != null)
         {
-            CJ.breakForce = 1000;
+            CJ.breakForce = 2500;
         }
+        Rigidbody _rb = Content.GetComponent<Rigidbody>();
+        _rb.detectCollisions = true;
+        _rb.angularDrag = 0.05f;
+        _rb.mass = 1;
     }
 
     public void OnGrabbed()
     {
         RemovablePart.enabled = true;
+    }
+
+    public void OnRemovableGrabbed()
+    {
         ConfigurableJoint CJ = RemovablePart.GetComponent<ConfigurableJoint>();
         if (CJ != null)
         {
-            CJ.breakForce = 1000;
+            CJ.breakForce = 2500;
         }
     }
 
     public void OnReleased()
     {
         RemovablePart.enabled = isUnpacked;
+    }
+
+    public void OnRemovableReleased()
+    {
         ConfigurableJoint CJ = RemovablePart.GetComponent<ConfigurableJoint>();
         if (CJ != null)
         {
