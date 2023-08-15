c52c621aa8ec51a364c1703b15e2b978970b4427
Roman <murman89@yandex.ru>
2023-08-15 13:37:49 +0300

    Restart system in multiplayer

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,23 +1,43 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
     SceneLoader sceneLoader;
+    public Action restart;
     private void Start()
     {
         sceneLoader = FindObjectOfType<SceneLoader>();
     }
     public void Fire(int i)
     {
+        if (PhotonManager.offlineMode)
+        {
+            if (sceneLoader != null) sceneLoader.LoadScene();
 
-        if (sceneLoader != null) sceneLoader.LoadScene();
+            if (i == -1)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            else
+                SceneManager.LoadScene(i);
+        }
+        if (!PhotonManager.offlineMode && i==-1)
+        {
+            if (i == -1)
+            {
+                PhotonManager.roomName = PhotonNetwork.CurrentRoom.Name;
+                PhotonManager.restart = true;
+            }
+            PhotonNetwork.DestroyPlayerObjects(PhotonNetwork.LocalPlayer);
+            PhotonNetwork.LeaveRoom();
+            SceneManager.LoadScene(0);
 
-        if(i == -1)
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        else
-            SceneManager.LoadScene(i);
+            restart?.Invoke();
+        }
+       
+
     }
 }
