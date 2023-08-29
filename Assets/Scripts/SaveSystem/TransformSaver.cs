0a1c72cc4e6d8c159525a26a04804d60cae66f09
Oleg-A-LLIto <Fedol1408@gmail.com>
2023-08-29 12:46:20 +0300

    Save Rigidbody info (kinematic)

--- Assets/Scripts/SaveSystem/TransformSaver.cs
+++ Assets/Scripts/SaveSystem/TransformSaver.cs
@@ -8,6 +8,13 @@ public class TransformSaver : DataSaver
     Vector3 LocalEulers;
     Vector3 LocalScale;
     Transform Parent;
+    bool Kinematic;
+    Rigidbody rb;
+
+    private void Start()
+    {
+        TryGetComponent<Rigidbody>(out rb);
+    }
 
     override public void Save()
     {
@@ -16,6 +23,8 @@ public class TransformSaver : DataSaver
         LocalEulers = transform.localEulerAngles;
         LocalScale = transform.localScale;
         Parent = transform.parent;
+        if (rb != null)
+            Kinematic = rb.isKinematic;
     }
 
     override public void Load()
@@ -25,5 +34,7 @@ public class TransformSaver : DataSaver
         transform.localPosition = LocalPosition;
         transform.localEulerAngles = LocalEulers;
         transform.localScale = LocalScale;
+        if (rb != null)
+            rb.isKinematic = Kinematic;
     }
 }
